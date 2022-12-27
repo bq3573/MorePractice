01a8df7f4f532e7d78e882048b307157f15dfab7
Brandon_Quinn <brandon8quinn@gmail.com>
2022-12-27 10:41:36 -0500

    added switch case method in Program: 12/27/2022

--- MorePractice/Program.cs
+++ MorePractice/Program.cs
@@ -33,11 +33,44 @@ namespace Program
 
             Console.WriteLine("Total Cost: $" + del(gifts));
 
-
+            Console.WriteLine("This is the most expensive item:\n");
             Console.WriteLine(most.item.Item1 + ": " + most.item.Item2);
+
+
+            // Implementation of a simple gift list to create a list of the items you want to buy and print them to
+            // a text document titled: GiftsToGet.txt
+            CreateShoppingList(gifts);
 
         }
 
+
+        public static void CreateShoppingList(List<Gift> gifts)
+        {
+            string fullpath = @"C:\Users\brand\source\repos\MorePractice\GiftsToGet.txt";
+            string giftsToBuy = "";
+            double sum = 0;
+            foreach (Gift gift in gifts)
+            {
+                Console.WriteLine(gift.item.Item1 + ": " + gift.item.Item2);
+                Console.WriteLine("Press 1 if you want to buy, Press 2 if you don't");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        giftsToBuy += gift.item.Item1 + ": " + gift.item.Item2 + "\n";
+                        sum += gift.item.Item2;
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+            File.WriteAllText(fullpath, giftsToBuy + "Total Cost: $" + sum);
+            // return giftsToBuy;
+        }
 
         // Sum total cost of gofts, meant for practicing delegates
         public static double SumGifts(List<Gift> gifts)
